9a28223b6461d7a8b8553f6e3acf1ae36d124174
julydsp <113667416+julydsp@users.noreply.github.com>
2023-10-20 13:26:13 +0700

    menambahkan konfigurasi font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      win: ["Inter", "sans-serif"],
+      logo: ["Jockey One", "sans-serif"],
+    },
   },
   plugins: [],
 };
