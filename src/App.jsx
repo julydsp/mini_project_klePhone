c4176f23822025fd15ce33b43dbafd71ffa52fe0
julydsp <113667416+julydsp@users.noreply.github.com>
2023-10-21 03:28:26 +0700

    membuat halaman tambah produk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import ProductPage from "./pages/ProductPage";
 import HomePage from "./pages/HomePage";
+import AddProductPage from "./pages/addProductPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/productPage" element={<ProductPage />}></Route>
+        <Route path="/addproductPage" element={<AddProductPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
